dbd627f560b44a09414c6008e50d3edb579dc7ea
darshangujraticodes <darshangujrati5@gmail.com>
2024-10-14 19:15:14 +0530

    linkedlist

--- JS-DSA-Course/Scott-Berett-Course/LinkedinList.js
+++ JS-DSA-Course/Scott-Berett-Course/LinkedinList.js
@@ -1,3 +1,21 @@
-class Node {}
+// why we are creating 2 class because create new Node is common code use in all push, insert and unshift and LinkedList direct code
 
-class LinkedList {}
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = newNode;
+    this.length = 1;
+  }
+}
+
+let linkedList = new LinkedList(5);
+
+console.log(linkedList);
