17a0898be910c8617ee2233c4192c119e09e2775
darshangujraticodes <darshangujrati5@gmail.com>
2024-10-17 18:58:16 +0530

    lp

--- JS-DSA-Course/Scott-Berett-Course/LinkedinList.js
+++ JS-DSA-Course/Scott-Berett-Course/LinkedinList.js
@@ -14,8 +14,27 @@ class LinkedList {
     this.tail = newNode;
     this.length = 1;
   }
+
+  push(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    this.length++;
+
+    return this;
+  }
 }
 
 let linkedList = new LinkedList(5);
+linkedList.push(45);
+linkedList.push(56);
+// linkedList.push(86);
 
 console.log(linkedList);
